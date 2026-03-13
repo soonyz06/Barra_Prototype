308888265c631ed5b9ab34f72c952512994e8df8
soonyz06 <soon.yz061025@gmail.com>
2026-03-13 16:15:16 +1100

    Add files via upload

--- Barra/main.py
+++ Barra/main.py
@@ -46,68 +46,41 @@ class History:
         history = pl.concat([history, missing_df])
         history.write_parquet(self.filepath)
         return history
-    
-    def add_pct_change(self, factor, df, period, offset=None, k=1):
-        #rolling raw pct change with validation within buffer_days 
-        #start: t+offset, end: t+offset+period, default offset = -period
-        buffer_days = 5
 
-        period_num, period_unit = self.split_date(period)
-        if offset is None: offset = f"0{period_unit}"
-        offset_num, offset_unit = self.split_date(offset)
-        assert period_unit == offset_unit, "Both periods should be the same"
-        period = f"{int(period_num)-int(offset_num)}{period_unit}"
-        offset = f"{-int(period_num)}{offset_unit}"
-        
-        df = df.rolling( 
-            index_column="date",
-            period=period,
-            offset=offset,
-            group_by="symbol"
-        ).agg([
-            ((pl.col("close").last()/pl.col("close").first() -1) * k).alias(factor),
-            (pl.col("date").first().alias("actual_start")),
-            (pl.col("date").last().alias("actual_end"))
-        ])
-
-        df = df.with_columns([
-            (pl.col("date").dt.offset_by(offset).alias("target_start"))
-        ]).with_columns([
-            (pl.col("target_start").dt.offset_by(period).alias("target_end"))
-        ])
-        
-        df = df.with_columns(
-            pl.when(
-                ((pl.col("actual_start")-pl.col("target_start")).dt.total_days().abs()<=buffer_days)
-                &
-                ((pl.col("actual_end")-pl.col("target_end")).dt.total_days().abs()<=buffer_days)
+    def log_transform(self, df):
+        return (
+            df.sort(self.identifiers)
+            .upsample(time_column="date", every="1d", group_by="symbol")
+            .with_columns(
+                pl.col("close").forward_fill().over("symbol") 
             )
-            .then(pl.col(factor))
-            .otherwise(None)
-            .alias(factor)
-        ).select(self.identifiers+[factor])
-        return df
+            .lazy()
+            .filter(pl.col("date").dt.weekday()<=5) #trading days only
+            .with_columns(
+                log_ret = pl.col("close").log().diff().over("symbol").fill_null(0)
+            )
+        )
 
-    def split_date(self, date):
-        match = re.match(r"(-?\d+)(.*)", date)
-        if not match:
-            return 0, date
-        return match.groups()   
+    def add_log_change(self, factor, df, lookback_days, gap_days):
+        #ln(a/b) = ln(a)-ln(b) (additive)
+        #ln: 10->20 == 10-> 5 (symmetric)
+        min_obs =  0.8
+        window_days = int(lookback_days - gap_days)
+        
+        return (
+            df.with_columns(
+                pl.col("log_ret")
+                .shift(gap_days) #pushed forward in time 
+                .rolling_sum(
+                    window_size=window_days,
+                    weights=None,
+                    min_samples=int(window_days*min_obs) #min_obs
+                )
+                .over("symbol")
+                .alias(factor)            
+            )
+        )
                    
-    def create_UMD(self, df, period, offset=None):
-        factor = f"UMD"
-        factor = f"{factor}_{period}_{offset}" if offset is not None else f"{factor}_{period}"
-        
-        df = self.add_pct_change(factor, df, period, offset, k=1)
-        return df
-
-    def create_HML(self, df, period, offset=None):
-        factor = f"HML"
-        factor = f"{factor}_{period}_{offset}" if offset is not None else f"{factor}_{period}"
-        
-        df = self.add_pct_change(factor, df, period, offset, k=-1)
-        return df
-
     def winsor_factor(self, df, factor, p=0.01):
         df = df.with_columns(
             pl.col(factor).clip(
@@ -130,58 +103,33 @@ class History:
         n = len(factors)
         if weights is None: weights = [1/n for i in range(n)]
 
-        combined_expr = sum(pl.col(f)*w for f, w in zip(factors, weights))
         df = df.with_columns([
-            combined_expr.alias(name)
-        ])
+            pl.sum_horizontal([pl.col(f)*w for f, w in zip(factors, weights)])
+        ]).alias(name)
         return self.znorm_factor(df, name)
         
 
 symbols = ["AAPL", "META", "MSFT", "WMT", "COST", "NVDA"]
 
 
-
+mo = 21 #trading days per month
 his = History()
 df = his.load_history(symbols)
+df = his.log_transform(df)
+df = his.add_log_change("UMD_12_1", df, 12*mo, mo)
 
-umd_configs = [("12mo", "0mo"), ("12mo", "1mo"), ("6mo", "0mo"), ("6mo", "1mo")]
-hml_configs = [("5y", None), ("3y", None)]
+df = df.collect()
+print(df[["date", "log_ret", "close", "UMD_12_1"]].tail())
 
-factor_dfs = []
-for p, o in umd_configs:
-    factor_dfs.append(his.create_UMD(df, p, o))
-for p, o in hml_configs:
-    factor_dfs.append(his.create_HML(df, p, o))
-    
-processed_dfs = []
-for f_df in factor_dfs:
-    f_name = f_df.columns[-1]
-    proc = (
-        f_df
-        .pipe(his.winsor_factor, f_name)
-        .pipe(his.znorm_factor, f_name)
-    )
-    processed_dfs.append(proc)
-
-factor_df = reduce( #reduces processed_dfs to factor_df
-    lambda left, right: left.join(right, on=["symbol", "date"], how="left"), 
-    processed_dfs
-)
-df = df.join(factor_df, on=["symbol", "date"], how="left")
+#Factor Neutralisation
+#F=BX+e, where F is raw factor, X is risk factors and B is estimated
+#zscore composure -> neut -> rescale (x-0)/sd: x/x.std().over("date")
+#OLS equation :)
 
 
-df = df.sort("date", "symbol")
-print(df.tail())
-
-
-
-
-
-
-#log returns are additive -> rolling sum of log returns
-
+#high, low masking
+#unit test, smooth forward fill
 #sampling
-
 
 
 
